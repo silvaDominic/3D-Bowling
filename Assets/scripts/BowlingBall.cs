5e6d079f32412b244775a1faa93474c1a1a4d5e9
reclaimer <silva.d.dominic@gmail.com>
2018-01-16 12:51:10 -0500

    Created prefabs for primary game objects -- Added simple physics and colliders to pins and ball -- Added tracking to camera and alt camera for viewing pins

---
+++ Assets/scripts/BowlingBall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BowlingBall : MonoBehaviour {
+
+    private Rigidbody rigidbody;
+    public Vector3 launchVelocity;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody>();
+
+        LaunchBall();
+	}
+
+    public void LaunchBall() {
+        rigidbody.velocity = launchVelocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.GetComponent<Floor>()) {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
---
+++ Assets/scripts/CameraControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public BowlingBall bowlingball;
+    private Vector3 offset;
+
+    // Use this for initialization
+    void Start () {
+        bowlingball = GameObject.FindObjectOfType<BowlingBall>();
+
+        offset = gameObject.transform.position - bowlingball.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // If ball is in front of head pin
+        if (bowlingball.transform.position.z <= 1829f) {
+            gameObject.transform.position = bowlingball.transform.position + offset;
+        }
+	}
+}
