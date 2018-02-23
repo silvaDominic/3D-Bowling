8a211eb03c03d677bd927988ba691c7b6ae52e53
reclaimer <silva.d.dominic@gmail.com>
2018-02-23 16:37:35 -0500

    Started writing unit tests

---
+++ Assets/scripts/ActionMaster.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster {
+    
+    public enum Action { Tidy, Reset, EndTurn, EndGame};
+
+    public Action Bowl(int pins) {
+
+        if (pins > 10 || pins < 0) {
+            throw new UnityException("Invalid pin count argument");
+        }
+
+        if (pins == 10) {
+            return Action.EndTurn;
+        }
+
+        throw new UnityException("Not sure what action to return.");
+    }
+}
---
+++ Assets/scripts/Editor/ActionMasterTest.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class ActionMasterTest {
+
+    
+    private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+
+	// A UnityTest behaves like a coroutine in PlayMode
+	// and allows you to yield null to skip a frame in EditMode
+	[UnityTest]
+	public IEnumerator ScoreMasterTestWithEnumeratorPasses() {
+		// Use the Assert class to test conditions.
+		// yield to skip a frame
+		yield return null;
+	}
+
+    [Test]
+    public void T01_OneStrikeReturnsEndTurn() {
+        ActionMaster actionMaster = new ActionMaster();
+        Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+}
