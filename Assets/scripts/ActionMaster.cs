00c5ab312032615ef3e357d284335ef68408b32f
reclaimer <silva.d.dominic@gmail.com>
2018-02-25 23:45:00 -0500

    Finished first round of unit tests for ActionMaster class

--- Assets/scripts/ActionMaster.cs
+++ Assets/scripts/ActionMaster.cs
@@ -4,18 +4,92 @@ using UnityEngine;
 
 public class ActionMaster {
     
-    public enum Action { Tidy, Reset, EndTurn, EndGame};
+    public enum Action {Tidy, Reset, EndTurn, EndGame};
+    public enum BowlResult {Strike, Spare, UnderTen};
+
+    private BowlResult previousBowl;
+    private Action actionResult;
+    private int[] balls = new int[21];
+    private int ball = 1;
 
     public Action Bowl(int pins) {
+
+        // Add pins to bowl array
+        AddToBowls(pins);
+
+        // Always return End Game on ball 21
+        if (ball == 21) {
+            this.actionResult = Action.EndGame;
+        }
+
+        switch (CheckBowlResult(pins)) {
+            case BowlResult.Strike:
+                if (this.ball >= 19 && this.ball < 21) {
+                    this.actionResult = Action.Reset;
+                } else if (this.ball < 19) {
+                    this.actionResult = Action.EndTurn;
+                }
+                previousBowl = BowlResult.Strike;
+                break;
+
+            case BowlResult.Spare:
+                if (this.ball == 20) {
+                    actionResult = Action.Reset;
+                } else if (this.ball < 20) {
+                    actionResult = Action.EndTurn;
+                }
+                previousBowl = BowlResult.Spare;
+                break;
+
+            case BowlResult.UnderTen:
+                if (this.ball % 2 != 0 && this.ball != 21) { // If middle of frame
+                    actionResult = Action.Tidy;
+                } else if (this.ball % 2 == 0) {  // If end of frame
+                    if (this.ball == 20) {
+                        if (previousBowl == BowlResult.Strike) {
+                            actionResult = Action.Tidy;
+                        } else {
+                            actionResult = Action.EndGame;
+                            }
+                    } else {
+                        actionResult = Action.EndTurn;
+                    }
+                }
+                previousBowl = BowlResult.UnderTen;
+                break;
+
+            default:
+                throw new UnityException("Not sure what action to return.");
+        }
+
+        this.ball++;
+        return actionResult;
+    }
+
+    private BowlResult CheckBowlResult(int pins) {
 
         if (pins > 10 || pins < 0) {
             throw new UnityException("Invalid pin count argument");
         }
 
         if (pins == 10) {
-            return Action.EndTurn;
+            return BowlResult.Strike;
         }
 
-        throw new UnityException("Not sure what action to return.");
+        return CheckForSpare(pins);
+    }
+
+    private BowlResult CheckForSpare(int pins) {
+        if (this.ball > 1) {
+            int previousBall = this.balls[this.ball - 2];
+            if (pins + previousBall == 10) {
+                return BowlResult.Spare;
+            }
+        }
+        return BowlResult.UnderTen;
+    }
+
+    private void AddToBowls(int pins) {
+        this.balls[this.ball - 1] = pins;
     }
 }
--- Assets/scripts/Editor/ActionMasterTest.cs
+++ Assets/scripts/Editor/ActionMasterTest.cs
@@ -6,21 +6,115 @@ using System.Collections;
 
 public class ActionMasterTest {
 
-    
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action endGame = ActionMaster.Action.EndGame;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+    private ActionMaster actionMaster;
 
-	// A UnityTest behaves like a coroutine in PlayMode
-	// and allows you to yield null to skip a frame in EditMode
-	[UnityTest]
-	public IEnumerator ScoreMasterTestWithEnumeratorPasses() {
-		// Use the Assert class to test conditions.
-		// yield to skip a frame
-		yield return null;
-	}
+    // A UnityTest behaves like a coroutine in PlayMode
+    // and allows you to yield null to skip a frame in EditMode
+    [UnityTest]
+    public IEnumerator ScoreMasterTestWithEnumeratorPasses() {
+        // Use the Assert class to test conditions.
+        // yield to skip a frame
+        yield return null;
+    }
 
+
+    [SetUp]
+    public void SetUp() {
+        actionMaster = new ActionMaster();
+    }
+
+    // ------------------- REGULAR FRAME SCENARIOS ------------------------
+    // ** Note UT --> Under-ten **
+
+    //Bowl an under-ten on a regular frame
+    // Returns tidy
     [Test]
-    public void T01_OneStrikeReturnsEndTurn() {
-        ActionMaster actionMaster = new ActionMaster();
+    public void T01_BowlUTReturnsTidy() {
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    // Strike on regular frame
+    // Returns end-turn
+    [Test]
+    public void T02_OneStrikeReturnsEndTurn() {
         Assert.AreEqual(endTurn, actionMaster.Bowl(10));
+    }
+
+    // Bowl a spare on a regular frame
+    // Returns tidy, end-turn
+    [Test]
+    public void T03_BowlSpareReturnsEndTurn() {
+        Assert.AreEqual(tidy, actionMaster.Bowl(2));
+        Assert.AreEqual(endTurn, actionMaster.Bowl(8));
+    }
+
+    // --------------------- LAST FRAME SCENARIOS ------------------------
+
+    // Under-ten on last frame
+    // Returns tidy and endGame
+    [Test]
+    public void T04_LastFrame_UTUT() {
+        int[] balls = { 3, 4, 6, 2, 4, 1, 3, 5, 2, 4, 2, 2, 1, 4, 5, 3, 6, 1 };
+        foreach (int ball in balls) {
+            actionMaster.Bowl(ball);
+        }
+        Assert.AreEqual(tidy, actionMaster.Bowl(3));
+        Assert.AreEqual(endGame, actionMaster.Bowl(4));
+    }
+
+    // Strike on last frame 10-1, under-ten on last frame 10-2 & 10-3
+    // Returns reset, tidy, endGame
+    [Test]
+    public void T05_LastFrame_StrikeUTUT() {
+        int[] balls = { 3, 4, 6, 2, 4, 1, 3, 5, 2, 4, 2, 2, 1, 4, 5, 3, 6, 1 };
+        foreach (int ball in balls) {
+            actionMaster.Bowl(ball);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(tidy, actionMaster.Bowl(3));
+        Assert.AreEqual(endGame, actionMaster.Bowl(4));
+    }
+
+    // Spare on last frame 10-1, strike on last frame 10-3
+    // Returns tidy, reset, and endgame
+    [Test]
+    public void T06_LastFrame_SpareStrike() {
+        int[] balls = { 3, 4, 6, 2, 4, 1, 3, 5, 2, 4, 2, 2, 1, 4, 5, 3, 6, 1 };
+        foreach (int ball in balls) {
+            actionMaster.Bowl(ball);
+        }
+        Assert.AreEqual(tidy, actionMaster.Bowl(3));
+        Assert.AreEqual(reset, actionMaster.Bowl(7));
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
+    }
+
+    // Strike on last frame, spare on last frame 10-3
+    // Returns reset, tidy, and end game
+    [Test]
+    public void T07_LastFrame_StrikeSpare() {
+        int[] balls = { 3, 4, 6, 2, 4, 1, 3, 5, 2, 4, 2, 2, 1, 4, 5, 3, 6, 1};
+        foreach (int ball in balls) {
+            actionMaster.Bowl(ball);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(tidy, actionMaster.Bowl(7));
+        Assert.AreEqual(endGame, actionMaster.Bowl(3));
+    }
+
+    // All strikes on last frame
+    // Returns reset, reset, end game
+    [Test]
+    public void T08_LastFrame_StrikeStrikeStrike() {
+        int[] balls = { 3, 4, 6, 2, 4, 1, 3, 5, 2, 4, 2, 2, 1, 4, 5, 3, 6, 1 };
+        foreach (int ball in balls) {
+            actionMaster.Bowl(ball);
+        }
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(reset, actionMaster.Bowl(10));
+        Assert.AreEqual(endGame, actionMaster.Bowl(10));
     }
 }
